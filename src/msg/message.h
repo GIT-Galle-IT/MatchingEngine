67c88a6964ee0792a6c8e43e25f9b153d8f8b730
MaheshMadushan <madu140@gmail.com>
2025-04-06 13:28:54 +0530

    ADD[Networking] - Tradin client and matching engine server comm skeleton

--- src/msg/message.h
+++ src/msg/message.h
@@ -11,38 +11,36 @@ struct OrderMessage
     int orderQuantity;
     int price;
 
-    OrderMessage(std::string instrumentID_, int orderType_, int orderQuantity_, int price_) :
-        instrumentID(instrumentID_), 
-        orderType(orderType_), 
-        orderQuantity(orderQuantity_), 
-        INSTRUMENT_ID_LENGTH(instrumentID_.size()), 
-        price(price_) {}
+    OrderMessage(std::string instrumentID_, int orderType_, int orderQuantity_, int price_) : instrumentID(instrumentID_),
+                                                                                              orderType(orderType_),
+                                                                                              orderQuantity(orderQuantity_),
+                                                                                              INSTRUMENT_ID_LENGTH(instrumentID_.size()),
+                                                                                              price(price_) {}
 
-    void serialize(std::ostream& os)
+    void serialize(std::ostream &os)
     {
-        os.write(reinterpret_cast<const char*>(&orderType), sizeof(orderType));
-        os.write(reinterpret_cast<const char*>(&orderQuantity), sizeof(orderQuantity));
-        os.write(reinterpret_cast<const char*>(&INSTRUMENT_ID_LENGTH), sizeof(INSTRUMENT_ID_LENGTH));
+        os.write(reinterpret_cast<const char *>(&orderType), sizeof(orderType));
+        os.write(reinterpret_cast<const char *>(&orderQuantity), sizeof(orderQuantity));
+        os.write(reinterpret_cast<const char *>(&INSTRUMENT_ID_LENGTH), sizeof(INSTRUMENT_ID_LENGTH));
         os.write(instrumentID.c_str(), instrumentID.length());
-        os.write(reinterpret_cast<const char*>(&price), sizeof(price));
+        os.write(reinterpret_cast<const char *>(&price), sizeof(price));
     }
 
-    void deserialize(std::istream& is)
+    void deserialize(std::istream &is)
     {
-        is.read(reinterpret_cast<char*>(&orderType), sizeof(orderType));
-        is.read(reinterpret_cast<char*>(&orderQuantity), sizeof(orderQuantity));
-        is.read(reinterpret_cast<char*>(&INSTRUMENT_ID_LENGTH), sizeof(INSTRUMENT_ID_LENGTH));
+        is.read(reinterpret_cast<char *>(&orderType), sizeof(orderType));
+        is.read(reinterpret_cast<char *>(&orderQuantity), sizeof(orderQuantity));
+        is.read(reinterpret_cast<char *>(&INSTRUMENT_ID_LENGTH), sizeof(INSTRUMENT_ID_LENGTH));
         char buffer[INSTRUMENT_ID_LENGTH];
         buffer[INSTRUMENT_ID_LENGTH] = '\0';
         is.read(buffer, INSTRUMENT_ID_LENGTH);
         instrumentID = buffer; // copy assign
-        is.read(reinterpret_cast<char*>(&price), sizeof(price));
+        is.read(reinterpret_cast<char *>(&price), sizeof(price));
     }
 };
 
-std::ostream& operator<<(std::ostream& os, OrderMessage message)
+std::ostream &operator<<(std::ostream &os, OrderMessage message)
 {
-    os << message.instrumentID << " " << message.orderType << " " << 
-        message.orderQuantity << " " << message.price;
+    os << message.instrumentID << " " << message.orderType << " " << message.orderQuantity << " " << message.price;
     return os;
 }
