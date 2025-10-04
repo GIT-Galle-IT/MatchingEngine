3bdef6c4502453b5a7477b5fc4e505d5a9dd0c35
Mahesh Jayawardhana <58337761+MaheshMadushan@users.noreply.github.com>
2025-10-04 05:31:32 +0000

    FIX[build] - Remove unnecessary Boost debug setting and streamline Boost package search in CMakeLists.txt

---
+++ gbase/gLog.h
@@ -0,0 +1,20 @@
+#if __cplusplus < 202302L
+#error This project requires C++23 or higher!
+#else
+#include <print>
+#ifdef DEBUG_LOG
+#define GLOG(formatted_log, ...)                                \
+    {                                                           \
+        std::println(formatted_log __VA_OPT__(, ) __VA_ARGS__); \
+    }
+#else
+#define GLOG(formatted_log, ...) // do nothing
+#endif
+
+#define GLOG_ERROR(formatted_log, ...)                                                                \
+    {                                                                                                 \
+        std::println("ERROR at {}:{} " formatted_log, __FILE__, __LINE__ __VA_OPT__(, ) __VA_ARGS__); \
+    }
+#endif
+
+#define GLOG(formatted_log, ...) // do nothing
--- gbase/logging/gLog.h
+++ gbase/logging/gLog.h
@@ -1,9 +1,21 @@
-#ifdef DEBUG_LOG
+#if __cplusplus < 202302L
+#warning This project requires C++23 or higher!
+#define GLOG(formatted_log, ...) // do nothing
+#else
 #include <print>
-#define GLOG(formatted_log, ...) \
-{                                      \
-    std::println(formatted_log __VA_OPT__(,) __VA_ARGS__);     \
-}
+#ifdef DEBUG_LOG
+#define GLOG(formatted_log, ...)                                \
+    {                                                           \
+        std::println(formatted_log __VA_OPT__(, ) __VA_ARGS__); \
+    }
 #else
 #define GLOG(formatted_log, ...) // do nothing
 #endif
+
+#define GLOG_ERROR(formatted_log, ...)                                                                \
+    {                                                                                                 \
+        std::println("ERROR at {}:{} " formatted_log, __FILE__, __LINE__ __VA_OPT__(, ) __VA_ARGS__); \
+    }
+#endif
+
+
