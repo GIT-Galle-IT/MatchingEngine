0ed9a235fae67181201a99b73fe0b3a15192462c
MaheshMadushan <madu140@gmail.com>
2025-11-29 09:08:32 +0530

    protocol architectural diagrams

--- gbase/net/gProtocol.hpp
+++ gbase/net/gProtocol.hpp
@@ -5,13 +5,35 @@ constexpr int __G_PROTOCOL_MAJOR_VERSION__ = 1;
             supports application level message delivery via TCP/IP protocol.
             Defines how to identify start and end of message byte stream
 
+        +----------------------------------------------------------+
+        |               (Application Layer) gProtocol              |
+        +----------------------------------------------------------+
+        |                         TCP                              |
+        +----------------------------------------------------------+
+        |                    internet protocol                     |
+        +----------------------------------------------------------+
+        |                  communication network                   |
+        +----------------------------------------------------------+
+
         Frame
             - header - 1 byte
                 - start of message  = 0x1
                 - message partition = 0x2
                 - end of message    = 0x3
-            - packet - size of data
-                - data
+            - packet 
+                - size of data - 16 bytes
+                - data - (size of data) bytes
+
+                 0                   1
+                 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 
+                +-+-+-+-+-+-+-+-+
+                |     header    |                
+                +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+                |         size of data          | 
+                +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+                ~            data               ~
+                +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        
 */
 namespace gbase::net::gProtocol
 {
